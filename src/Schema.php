7e942686bf5879fe7e388ceaccab632e50f52a19
Christopher Gammie <contact@gammie.co.uk>
2020-11-13 14:12:27 +0000

    [Refactor] Move schema resolving logic to abstract schema

--- src/Schema.php
+++ src/Schema.php
@@ -21,27 +21,14 @@ namespace LaravelJsonApi\Eloquent;
 
 use Illuminate\Database\Eloquent\Model;
 use LaravelJsonApi\Contracts\Store\Repository as RepositoryContract;
-use LaravelJsonApi\Core\Resolver\ResourceClass;
-use LaravelJsonApi\Core\Resolver\ResourceType;
 use LaravelJsonApi\Core\Schema\Schema as BaseSchema;
 use LaravelJsonApi\Eloquent\Fields\ID;
-use LaravelJsonApi\Eloquent\Fields\Relations\BelongsTo;
 use LaravelJsonApi\Eloquent\Fields\Relations\ToMany;
 use LaravelJsonApi\Eloquent\Fields\Relations\ToOne;
 use LogicException;
 
 abstract class Schema extends BaseSchema
 {
-
-    /**
-     * @var callable|null
-     */
-    public static $resourceTypeResolver;
-
-    /**
-     * @var callable|null
-     */
-    protected static $resourceResolver;
 
     /**
      * The relationships that should always be eager loaded.
@@ -56,27 +43,6 @@ abstract class Schema extends BaseSchema
     private ?string $idColumn = null;
 
     /**
-     * Specify the callback to use to guess the resource type from the schema class.
-     *
-     * @param callable $resolver
-     * @return void
-     */
-    public static function guessTypeUsing(callable $resolver): void
-    {
-        static::$resourceTypeResolver = $resolver;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public static function type(): string
-    {
-        $resolver = static::$resourceResolver ?: new ResourceType();
-
-        return $resolver(static::class);
-    }
-
-    /**
      * @inheritDoc
      */
     public static function model(): string
@@ -86,27 +52,6 @@ abstract class Schema extends BaseSchema
         }
 
         throw new LogicException('The model class name must be set.');
-    }
-
-    /**
-     * Specify the callback to use to guess the resource class from the schema class.
-     *
-     * @param callable $resolver
-     * @return void
-     */
-    public static function guessResourceUsing(callable $resolver): void
-    {
-        static::$resourceResolver = $resolver;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public static function resource(): string
-    {
-        $resolver = static::$resourceResolver ?: new ResourceClass();
-
-        return $resolver(static::class);
     }
 
     /**
